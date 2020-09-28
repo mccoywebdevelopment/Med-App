0f8db283d03fa21efdc5726b95d041a7bfd32753
Christopher McCoy <cmmccoy1996@gmail.com>
2020-09-28 16:18:16 -0700

    transfer.

--- frontend/src/actions/user.js
+++ frontend/src/actions/user.js
@@ -69,100 +69,111 @@ export const fetchCreateUser = (body, groupID, done) => (dispatch) => {
 }
 
 export const fetchDeleteUser = (userID, done) => (dispatch) => {
-  FETCH('DELETE', '/users/' + userID + '/',null, localStorage.getItem('JWT'), dispatch, isLoading, (err, res) => {
+  dispatch(toggleLoading(false));
+  FETCH('DELETE', '/users/' + userID + '/',null, localStorage.getItem('JWT'), dispatch, false, (err, res) => {
     if (err) {
       dispatch(createMessage(err, 'danger'));
     }else {
-      dispatch(fetchGuardians(isLoading,(guardians)=>{
-        res = combineGuardian(res, guardians);
-        dispatch({
-          type: FETCH_USERS,
-          payload: res
-        });
-        if (done) {
-          done(res);
-        }
-      }));
+      dispatch(fetchUsers(false,(users)=>{
+        dispatch(createMessage(userBody.username + " was sent an invitation via email.", "info"));
+        dispatch(toggleLoading(false));
+          if(done){
+            done(res);
+          }
+        }));
     }
   });
 }
 
-export const fetchDeleteUser = (depID, done) => (dispatch) => {
-  fetch(API_URI + "/users/" + depID.toString() + "/" + localStorage.getItem('JWT'), {
-    method: 'DELETE',
-    headers: {
-      'content-type': 'application/json'
-    }
-  })
-    .then(res => res.json())
-    .then(res => {
-      if (res.error) {
-        dispatch(createMessage(res.error, 'danger'));
+export const fetchUpdateUser = (userID, username, body, isGroupModified, done) => (dispatch) => {
+  FETCH('POST', '/users/'+userID,body, localStorage.getItem('JWT'), dispatch, isLoading, (err, res) => {
+    if (err) {
+      dispatch(createMessage(err, 'danger'));
+    }else if(isGroupModified){
+      if (isGroupModified.isAdd) {
+        dispatch(addUser(isGroupModified.groupID,res._id,false,(guardianAdded)=>{
+          updateUserAfter(dispatch,username,done,res);
+        }));
+      } else if (isGroupModified.isRemoved) {
+        dispatch(removeDependent(isGroupModified.groupID,res._id,false,(depAdded)=>{
+          updateDependentAfter(dispatch,name,done,res);
+        }));
       } else {
-        dispatch(createMessage(res.username + " was successfully deleted.", "info"));
-        dispatch(fetchUsers());
-      }
-      if (done) {
-        done(res);
-      }
-    });
-};
-/*need to check if user is modified and then check if group isModified.*/
-export const fetchUpdateUser = (id,userBody,isGroupModified,guardians,guardian,done) => (dispatch) => {
-
-  let updateBody = {
-    updatedFields:userBody
-  }
-  dispatch(toggleLoading(true));
-  fetch(API_URI + "/users/"+id+"/"+localStorage.getItem('JWT'), {
-    method: 'PATCH',
-    headers: {
-      'content-type': 'application/json'
-    },
-    body: JSON.stringify(updateBody)
-  })
-    .then(res => res.json())
-    .then(res => {
-      dispatch(toggleLoading(false));
-      if (res.error) {
-        dispatch(createMessage(res.error, 'danger'));
-      } else {
-        /* Need */
-        if(isGroupModified){
-          if(!guardian){
-            dispatch(fetchCreateGuardian({user:id},(guardian)=>{
-              updateGroupData(dispatch,isGroupModified,guardian,guardians,()=>{
-                sendback(dispatch,res,id,(user)=>{
-                  done(user);
-                });
-              });
-            },true));
-          }else{
-            updateGroupData(dispatch,isGroupModified,guardian,guardians,()=>{
-              sendback(dispatch,res,id,(user)=>{
-                done(user);
-              });
-            });
-          }
-        }
-          dispatch(createMessage(res.username + " was successfully updated.","success"));
-          dispatch(fetchUsers(true,(users)=>{
-            let user = null;
-            if(done){
-              for(var i=0;i<users.length;++i){
-                if(users[i]._id == id){
-                  user = users[i];
-                }
-              }
-              if(!user){
-                alert("Error 3234299077424 Couldn't find user.")
-              }
-              done(user)
-            }
+        dispatch(removeDependent(isGroupModified.oldGroupID,res._id,false,(depAdded)=>{
+          dispatch(addDependent(isGroupModified.groupID, res._id, false,(depRemoved)=>{
+            updateDependentAfter(dispatch,name,done,res);
           }));
-        }
-    });
-};
+        }));
+      }
+    }
+  });
+}
+
+function updateUserAfter(dispatch,name,done,res){
+  dispatch(fetchUsers(false,(users)=>{
+    dispatch(createMessage( name + " was successfully updated.", "success"));
+    dispatch(toggleLoading(false));
+    if(done){
+      done(res)
+    }
+  }));
+}
+/*need to check if user is modified and then check if group isModified.*/
+// export const fetchUpdateUser = (id,userBody,isGroupModified,guardians,guardian,done) => (dispatch) => {
+
+//   let updateBody = {
+//     updatedFields:userBody
+//   }
+//   dispatch(toggleLoading(true));
+//   fetch(API_URI + "/users/"+id+"/"+localStorage.getItem('JWT'), {
+//     method: 'PATCH',
+//     headers: {
+//       'content-type': 'application/json'
+//     },
+//     body: JSON.stringify(updateBody)
+//   })
+//     .then(res => res.json())
+//     .then(res => {
+//       dispatch(toggleLoading(false));
+//       if (res.error) {
+//         dispatch(createMessage(res.error, 'danger'));
+//       } else {
+//         /* Need */
+//         if(isGroupModified){
+//           if(!guardian){
+//             dispatch(fetchCreateGuardian({user:id},(guardian)=>{
+//               updateGroupData(dispatch,isGroupModified,guardian,guardians,()=>{
+//                 sendback(dispatch,res,id,(user)=>{
+//                   done(user);
+//                 });
+//               });
+//             },true));
+//           }else{
+//             updateGroupData(dispatch,isGroupModified,guardian,guardians,()=>{
+//               sendback(dispatch,res,id,(user)=>{
+//                 done(user);
+//               });
+//             });
+//           }
+//         }
+//           dispatch(createMessage(res.username + " was successfully updated.","success"));
+//           dispatch(fetchUsers(true,(users)=>{
+//             let user = null;
+//             if(done){
+//               for(var i=0;i<users.length;++i){
+//                 if(users[i]._id == id){
+//                   user = users[i];
+//                 }
+//               }
+//               if(!user){
+//                 alert("Error 3234299077424 Couldn't find user.")
+//               }
+//               done(user)
+//             }
+//           }));
+//         }
+//     });
+// };
 
 function sendback(dispatch,res,id,callback){
   dispatch(createMessage(res.username + " was successfully updated.","success"));
