499d9b9a86b2863e0e770b403bb020f65ab08c09
cmmccoy1996@gmail.com <cmmccoy1996@gmail.com>
2021-01-24 10:43:13 -0700

    t

--- frontend/src/config/variables.js
+++ frontend/src/config/variables.js
@@ -3,7 +3,7 @@
 // export const API_URI = "http://localhost:4000/api"
 
 // testing mode:
-//export const API_URI = "https://med-app-testing.herokuapp.com/"
+export const API_URI = "https://med-app-testing.herokuapp.com/"
 
 // prod mode:
-export const API_URI = "https://med-app-v1.herokuapp.com/api";
+// export const API_URI = "https://med-app-v1.herokuapp.com/api";
