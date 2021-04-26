f521cc2400efe55516c59b4170fe3768b9b75ae7
Christopher McCoy <cmmccoy1996@gmail.com>
2021-04-26 14:16:28 -0700

    t

--- frontend/src/config/variables.js
+++ frontend/src/config/variables.js
@@ -5,5 +5,6 @@
 // testing mode:
 export const API_URI = "https://med-app-testing.herokuapp.com/api"
 
+
 // prod mode:
 // export const API_URI = "https://sunshine-acres-med-app.herokuapp.com/api";
