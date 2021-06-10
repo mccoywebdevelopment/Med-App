f971fe4164cbd41f4ef379fa22d71cb94775ffb4
Christopher McCoy <cmmccoy1996@gmail.com>
2021-06-10 11:02:04 -0700

    t

--- frontend/src/config/variables.js
+++ frontend/src/config/variables.js
@@ -1,11 +1,11 @@
 
 // dev mode:
-export const API_URI = "http://localhost:4000/api"
-export const CLIENT_URL = "http://localhost:3000"
+// export const API_URI = "http://localhost:4000/api"
+// export const CLIENT_URL = "http://localhost:3000"
 
 // testing mode:
-// export const API_URI = "https://med-app-testing.herokuapp.com/api"
-// export const CLIENT_URL = "https://med-app-testing.herokuapp.com"
+export const API_URI = "https://med-app-testing.herokuapp.com/api"
+export const CLIENT_URL = "https://med-app-testing.herokuapp.com"
 
 
 // prod mode:
